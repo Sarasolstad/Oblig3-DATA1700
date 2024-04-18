339ad0bcc9e6df67e5277077209f4b08c744e59f
Sarasolstad <sawes2713@oslomet.no>
2024-04-18 12:01:59 +0200

    hola como estas!

---
+++ src/main/java/com/example/oblig3sara/Oblig3SaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig3sara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3SaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3SaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig3sara/Oblig3SaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3sara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3SaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
